8c4e8151cd4ac9595385a7c29b003405482458c8
Davidoffili <palagashvilid@gmail.com>
2020-04-03 17:40:38 -0700

    sum function complete

--- app.js
+++ app.js
@@ -8,12 +8,14 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
-
+function sum(a, b) {
+    var getSum = (a + b);
+    var answerQ1 = 'The sum of ' + a + ' and ' + b + ' is ' + getSum + '.';
+    return [getSum, answerQ1];
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -70,7 +72,11 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+    var arrayTotal = 0;
+    
+    for(var i = 0; i < sumArr.length; i++); {
+        arrayTotal = sum()
+    }
 }
 
 // Here is the test for sumArray(); uncomment it to run it
@@ -91,7 +97,7 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+    
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
