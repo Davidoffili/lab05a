0315b9d1365b1da5860a0afc025ab39403a57841
Davidoffili <palagashvilid@gmail.com>
2020-04-04 19:34:29 -0700

    completed stretch goal!

--- app.js
+++ app.js
@@ -119,7 +119,7 @@ function multiplyArray(multArr) { //eslint-disable-line
 
 
 // Here is the test for multiplyArray(); uncomment it to run it
-testMultiplyArray(testArray);
+// testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
@@ -143,10 +143,16 @@ Test this function by hand in the console to get it working, and when you think 
 var testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
-
+    var arrayTotal = 1;
+    for (var i = 0; i < dynamicArray.length; i++) {
+        arrayTotal = multiply(arrayTotal, dynamicArray[i])[0]
+    }
+    console.log(arrayTotal);
+    var aboveBeyond = 'The numbers ' + dynamicArray + ' have a product of ' + arrayTotal + '.';
+    return[arrayTotal, aboveBeyond];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
